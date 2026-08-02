baf788444457ec9c0167116d91a6d41e168d9445
Template Janitor <template.janitor@example.com>
2026-08-02 23:52:54 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -6,7 +6,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(waiwaiDPI_supports_sanitizers)
   # Emscripten doesn't support sanitizers
   if(EMSCRIPTEN)
     set(SUPPORTS_UBSAN OFF)
@@ -57,110 +57,110 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(waiwaiDPI_setup_options)
+  option(waiwaiDPI_ENABLE_HARDENING "Enable hardening" ON)
+  option(waiwaiDPI_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    waiwaiDPI_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    waiwaiDPI_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  waiwaiDPI_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR waiwaiDPI_PACKAGING_MAINTAINER_MODE)
+    option(waiwaiDPI_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(waiwaiDPI_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(waiwaiDPI_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(waiwaiDPI_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(waiwaiDPI_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(waiwaiDPI_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(waiwaiDPI_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(waiwaiDPI_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(waiwaiDPI_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(waiwaiDPI_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(waiwaiDPI_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(waiwaiDPI_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(waiwaiDPI_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(waiwaiDPI_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(waiwaiDPI_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(waiwaiDPI_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(waiwaiDPI_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(waiwaiDPI_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(waiwaiDPI_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(waiwaiDPI_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(waiwaiDPI_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(waiwaiDPI_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(waiwaiDPI_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(waiwaiDPI_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      waiwaiDPI_ENABLE_IPO
+      waiwaiDPI_WARNINGS_AS_ERRORS
+      waiwaiDPI_ENABLE_SANITIZER_ADDRESS
+      waiwaiDPI_ENABLE_SANITIZER_LEAK
+      waiwaiDPI_ENABLE_SANITIZER_UNDEFINED
+      waiwaiDPI_ENABLE_SANITIZER_THREAD
+      waiwaiDPI_ENABLE_SANITIZER_MEMORY
+      waiwaiDPI_ENABLE_UNITY_BUILD
+      waiwaiDPI_ENABLE_CLANG_TIDY
+      waiwaiDPI_ENABLE_CPPCHECK
+      waiwaiDPI_ENABLE_COVERAGE
+      waiwaiDPI_ENABLE_PCH
+      waiwaiDPI_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  waiwaiDPI_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (waiwaiDPI_ENABLE_SANITIZER_ADDRESS OR waiwaiDPI_ENABLE_SANITIZER_THREAD OR waiwaiDPI_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(waiwaiDPI_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(waiwaiDPI_global_options)
+  if(waiwaiDPI_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    waiwaiDPI_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  waiwaiDPI_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(waiwaiDPI_ENABLE_HARDENING AND waiwaiDPI_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR waiwaiDPI_ENABLE_SANITIZER_UNDEFINED
+       OR waiwaiDPI_ENABLE_SANITIZER_ADDRESS
+       OR waiwaiDPI_ENABLE_SANITIZER_THREAD
+       OR waiwaiDPI_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${waiwaiDPI_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${waiwaiDPI_ENABLE_SANITIZER_UNDEFINED}")
+    waiwaiDPI_enable_hardening(waiwaiDPI_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(waiwaiDPI_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(waiwaiDPI_warnings INTERFACE)
+  add_library(waiwaiDPI_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  waiwaiDPI_set_project_warnings(
+    waiwaiDPI_warnings
+    ${waiwaiDPI_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
@@ -171,66 +171,66 @@ macro(myproject_local_options)
 
   if(NOT EMSCRIPTEN)
     include(cmake/Sanitizers.cmake)
-    myproject_enable_sanitizers(
-      myproject_options
-      ${myproject_ENABLE_SANITIZER_ADDRESS}
-      ${myproject_ENABLE_SANITIZER_LEAK}
-      ${myproject_ENABLE_SANITIZER_UNDEFINED}
-      ${myproject_ENABLE_SANITIZER_THREAD}
-      ${myproject_ENABLE_SANITIZER_MEMORY})
+    waiwaiDPI_enable_sanitizers(
+      waiwaiDPI_options
+      ${waiwaiDPI_ENABLE_SANITIZER_ADDRESS}
+      ${waiwaiDPI_ENABLE_SANITIZER_LEAK}
+      ${waiwaiDPI_ENABLE_SANITIZER_UNDEFINED}
+      ${waiwaiDPI_ENABLE_SANITIZER_THREAD}
+      ${waiwaiDPI_ENABLE_SANITIZER_MEMORY})
   endif()
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(waiwaiDPI_options PROPERTIES UNITY_BUILD ${waiwaiDPI_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(waiwaiDPI_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      waiwaiDPI_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(waiwaiDPI_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    waiwaiDPI_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(waiwaiDPI_ENABLE_CLANG_TIDY)
+    waiwaiDPI_enable_clang_tidy(waiwaiDPI_options ${waiwaiDPI_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(waiwaiDPI_ENABLE_CPPCHECK)
+    waiwaiDPI_enable_cppcheck(${waiwaiDPI_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(waiwaiDPI_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    waiwaiDPI_enable_coverage(waiwaiDPI_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(waiwaiDPI_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(waiwaiDPI_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(waiwaiDPI_ENABLE_HARDENING AND NOT waiwaiDPI_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR waiwaiDPI_ENABLE_SANITIZER_UNDEFINED
+       OR waiwaiDPI_ENABLE_SANITIZER_ADDRESS
+       OR waiwaiDPI_ENABLE_SANITIZER_THREAD
+       OR waiwaiDPI_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    waiwaiDPI_enable_hardening(waiwaiDPI_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
--- cmake/Linker.cmake
+++ cmake/Linker.cmake
@@ -1,20 +1,20 @@
-macro(myproject_configure_linker project_name)
-  set(myproject_USER_LINKER_OPTION
+macro(waiwaiDPI_configure_linker project_name)
+  set(waiwaiDPI_USER_LINKER_OPTION
     "DEFAULT"
       CACHE STRING "Linker to be used")
-    set(myproject_USER_LINKER_OPTION_VALUES "DEFAULT" "SYSTEM" "LLD" "GOLD" "BFD" "MOLD" "SOLD" "APPLE_CLASSIC" "MSVC")
-  set_property(CACHE myproject_USER_LINKER_OPTION PROPERTY STRINGS ${myproject_USER_LINKER_OPTION_VALUES})
+    set(waiwaiDPI_USER_LINKER_OPTION_VALUES "DEFAULT" "SYSTEM" "LLD" "GOLD" "BFD" "MOLD" "SOLD" "APPLE_CLASSIC" "MSVC")
+  set_property(CACHE waiwaiDPI_USER_LINKER_OPTION PROPERTY STRINGS ${waiwaiDPI_USER_LINKER_OPTION_VALUES})
   list(
     FIND
-    myproject_USER_LINKER_OPTION_VALUES
-    ${myproject_USER_LINKER_OPTION}
-    myproject_USER_LINKER_OPTION_INDEX)
+    waiwaiDPI_USER_LINKER_OPTION_VALUES
+    ${waiwaiDPI_USER_LINKER_OPTION}
+    waiwaiDPI_USER_LINKER_OPTION_INDEX)
 
-  if(${myproject_USER_LINKER_OPTION_INDEX} EQUAL -1)
+  if(${waiwaiDPI_USER_LINKER_OPTION_INDEX} EQUAL -1)
     message(
       STATUS
-        "Using custom linker: '${myproject_USER_LINKER_OPTION}', explicitly supported entries are ${myproject_USER_LINKER_OPTION_VALUES}")
+        "Using custom linker: '${waiwaiDPI_USER_LINKER_OPTION}', explicitly supported entries are ${waiwaiDPI_USER_LINKER_OPTION_VALUES}")
   endif()
 
-  set_target_properties(${project_name} PROPERTIES LINKER_TYPE "${myproject_USER_LINKER_OPTION}")
+  set_target_properties(${project_name} PROPERTIES LINKER_TYPE "${waiwaiDPI_USER_LINKER_OPTION}")
 endmacro()
