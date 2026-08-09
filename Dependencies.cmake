82131066d31ca90e1a3f9e41107a0b36215af011
klewy <cigankov807@gmail.com>
2026-08-10 09:31:57 +1000

    init project

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -8,18 +8,6 @@ function(waiwaiDPI_setup_dependencies)
   # For each dependency, see if it's
   # already been provided to us by a parent project
 
-  if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage(
-      NAME
-      fmt
-      GITHUB_REPOSITORY
-      "fmtlib/fmt"
-      GIT_TAG
-      "12.1.0"
-      SYSTEM
-      YES)
-  endif()
-
   if(NOT TARGET spdlog::spdlog)
     cpmaddpackage(
       NAME
@@ -42,30 +30,6 @@ function(waiwaiDPI_setup_dependencies)
       3.12.0
       GITHUB_REPOSITORY
       "catchorg/Catch2"
-      SYSTEM
-      YES)
-  endif()
-
-  if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage(
-      NAME
-      CLI11
-      VERSION
-      2.6.1
-      GITHUB_REPOSITORY
-      "CLIUtils/CLI11"
-      SYSTEM
-      YES)
-  endif()
-
-  if(NOT TARGET ftxui::screen)
-    cpmaddpackage(
-      NAME
-      FTXUI
-      VERSION
-      6.1.9
-      GITHUB_REPOSITORY
-      "ArthurSonzogni/FTXUI"
       SYSTEM
       YES)
   endif()
